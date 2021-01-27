2bf217c7601bc1875975806f9e00c30a50d5d238
Joshua Robbins <jrobbin@sandia.gov>
2021-01-27 12:14:46 -0700

    switched to exo2obj

--- src/routes/jobs/uploadexodusmodel.js
+++ src/routes/jobs/uploadexodusmodel.js
@@ -1,6 +1,6 @@
 const fs = require('fs'); // include file system module
 const router = require('express').Router();
-const runDir = './calculations/';
+const runDir = './calculations';
 const getUserTime = require('../../helpers/getUserTime');
 const {sse} = require('../../config/serverSentEvent');
 const {spawn} = require('child_process');
@@ -8,7 +8,7 @@ const {spawn} = require('child_process');
 router.post('/', function(req, res) {
   if (req.files !== null) {
     const exoFileName = `${req.body.username}-${getUserTime()}.exo`;
-    const exoFilePath = `${runDir}${exoFileName}`;
+    const exoFilePath = `${runDir}/${exoFileName}`;
     const modelName = req.files.file.name;
     fs.writeFile(exoFilePath, req.files.file.data, 'utf-8', function(err) {
       if (err) {
@@ -20,14 +20,13 @@ router.post('/', function(req, res) {
         sse.send({
           modelName: modelName,
           remoteName: exoFileName,
-          remotePath: runDir}, 'modelRemoteData');
+          remotePath: `${runDir}/`}, 'modelRemoteData');
 
         const eventName = 'modelGeometryData';
-        const scriptFileName = './scripts/exoToOBJ.py';
-        const convert = spawn('python', [scriptFileName, exoFilePath, runDir]);
+        const executableName = 'exo2obj';
+        const convert = spawn(executableName, [exoFilePath, runDir]);
 
         convert.stdout.on('data', (data) => {
-          const errStatus = 0;
           strData = `${data}`;
           entries = strData.split('\n').filter( (e) => e !== '' );
           typeArray = entries.map( (e) => e.split(':')[0] );
@@ -37,28 +36,23 @@ router.post('/', function(req, res) {
             fs.readFile( dataArray[iData], 'utf-8', function(err, data) {
               if (err) {
                 console.log(`read error: ${err}`);
-                errStats = 1;
               }
-              console.log(`read success. sending`);
               sse.send({
                 modelName: modelName,
                 type: typeArray[thisIndex],
                 name: dataArray[thisIndex],
                 remoteName: exoFileName,
-                remotePath: runDir,
+                remotePath: `${runDir}/`,
                 data: data}, eventName);
             });
-          }
-          if (errStatus == 0) {
-            res.status(200).send('SUCCESS');
-          } else {
-            res.status(200).send('FAILURE');
           }
         });
 
         convert.stderr.on('data', (data) => {
           console.log(`stderr: ${data}`);
         });
+
+        res.status(200).send('SUCCESS');
       }
     });
   };
