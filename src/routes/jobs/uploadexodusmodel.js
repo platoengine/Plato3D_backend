6fbb5ffef03a32dc184a8d1217cb9259d5b4efa8
Joshua Robbins <jrobbin@sandia.gov>
2021-02-08 22:01:32 -0700

    exodus upload accepts csm files

--- src/routes/jobs/uploadexodusmodel.js
+++ src/routes/jobs/uploadexodusmodel.js
@@ -7,25 +7,49 @@ const {spawn} = require('child_process');
 
 router.post('/', function(req, res) {
   if (req.files !== null) {
-    const exoFileName = `${req.body.username}-${getUserTime()}.exo`;
-    const exoFilePath = `${runDir}/${exoFileName}`;
     const modelName = req.files.file.name;
-    fs.writeFile(exoFilePath, req.files.file.data, 'utf-8', function(err) {
+    const tokens = modelName.split('.');
+    const fileExtension = tokens.pop();
+
+    const baseFileName = `${req.body.username}-${getUserTime()}`;
+    const baseFilePath = `${runDir}/${baseFileName}`;
+
+    const exoFileName = `${baseFileName}.exo`;
+    const csmFileName = `${baseFileName}.csm`;
+
+    let inputFilePath = '';
+    let command = '';
+    if (fileExtension === 'csm' || fileExtension === 'CSM') {
+      inputFilePath = `${baseFilePath}.csm`;
+      let csm2exoCommand = `plato-cli geometry esp `;
+      csm2exoCommand += `--input ${csmFileName} `;
+      csm2exoCommand += `--output-mesh ${exoFileName} `;
+      csm2exoCommand += `>/dev/null 2>&1`;
+
+      const exo2objCommand = `exo2obj ${exoFileName} ./`;
+      command = csm2exoCommand + ' && ' + exo2objCommand;
+    } else {
+      command = `exo2obj ${exoFileName} ./`;
+      inputFilePath = `${baseFilePath}.exo`;
+    }
+
+    console.log(`command: ${command}`);
+
+    fs.writeFile(inputFilePath, req.files.file.data, 'utf-8', function(err) {
       if (err) {
         res.status(200).send('FAILURE');
-        console.log('Failed to upload exodus model');
+        console.log('Failed to upload model');
       } else {
-        console.log('Uploaded exodus model');
+        console.log('Uploaded model');
 
         sse.send({
           modelName: modelName,
           remoteName: exoFileName,
           remotePath: `${runDir}/`}, 'modelRemoteData');
 
-        const eventName = 'modelGeometryData';
-        const executableName = 'exo2obj';
-        const convert = spawn(executableName, [exoFilePath, runDir]);
+        const convert = spawn(command, {shell: true, cwd: runDir});
 
+        const eventName = 'modelGeometryData';
         convert.stdout.on('data', (data) => {
           strData = `${data}`;
           entries = strData.split('\n').filter( (e) => e !== '' );
@@ -33,18 +57,20 @@ router.post('/', function(req, res) {
           dataArray = entries.map( (e) => e.split(':')[1] );
           for (iData=0; iData<entries.length; iData++) {
             const thisIndex = iData;
-            fs.readFile( dataArray[iData], 'utf-8', function(err, data) {
-              if (err) {
-                console.log(`read error: ${err}`);
-              }
-              sse.send({
-                modelName: modelName,
-                type: typeArray[thisIndex],
-                name: dataArray[thisIndex],
-                remoteName: exoFileName,
-                remotePath: `${runDir}/`,
-                data: data}, eventName);
-            });
+            fs.readFile( `${runDir}/${dataArray[iData]}`, 'utf-8',
+                function(err, data) {
+                  if (err) {
+                    console.log(`read error: ${err}`);
+                  }
+                  sse.send({
+                    modelName: modelName,
+                    type: typeArray[thisIndex],
+                    name: dataArray[thisIndex],
+                    remoteName: exoFileName,
+                    remotePath: `${runDir}/`,
+                    data: data}, eventName);
+                }
+            );
           }
         });
 
