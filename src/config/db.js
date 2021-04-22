57f86b467ec472fb6e8ae922939fb1cf43085776
Joshua Robbins <jrobbin@sandia.gov>
2021-04-22 12:32:10 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,7 @@
 const mongoose = require('mongoose');
-// use this line for remote mongodb
-// const mongoDB = 'mongodb://PlatoDeploy:leg0l0s@ds135747.mlab.com:35747/scene_data';
-
-// username: develop
-// password: Term!nat3d
-// mongodb+srv://develop:Term!nat3d@cluster0.dycrh.mongodb.net/cluster0-shard-00-02.dycrh.mongodb.net:27017
 
 // use this line for local mongodb w/ container
 const mongoDB = 'mongodb://mongo:27017/mongo-test';
-// const mongoDB = 'mongodb+srv://develop:Term!nat3d@cluster0.dycrh.mongodb.net';
-
-// this checks to see if the DATABASE_HOST environment variable is set, to e.g.:
-// "mongodb://mongo:27017/mongo-test"
-// and uses it if it is.
-// const mongoDB = process.env.DATABASE_HOST ? process.env.DATABASE_HOST : 'mongodb+srv://develop:Term!nat3d@cluster0.dycrh.mongodb.net';
-
 
 mongoose.connect(mongoDB, {useUnifiedTopology: true, useNewUrlParser: true});
 mongoose.Promise = global.Promise;
